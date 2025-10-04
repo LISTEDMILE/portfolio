4a75463a8cc726ac53f68c0a1229832a9baa372d
listedmile <listedmile@gmail.com>
2025-10-05 01:22:55 +0530

    routes

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Header from "./components/header";
+import Footer from "./components/Footer";
+import LandingPage from "./pages/LandingPage";
+import PortfolioPage from "./pages/protfolio";
+
+
+
+
+function Layout() {
+  return (<>
+    <Header />
+    <Outlet />
+    <Footer/>
+  </>)
+}
+
+function App() {
+
+
+  
+  const route = createBrowserRouter([
+    {
+      element: <Layout />,
+      children: [{
+      path:"/",element:<LandingPage/>
+      },
+      {path:"/portfolio",element:<PortfolioPage/>}
+      ]
+  }
+])
+  return (<RouterProvider router = { route }></RouterProvider>)
+}
+
+export default App
