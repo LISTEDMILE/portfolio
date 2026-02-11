67f3e3c50c7acddb057b7bc02a8f66f482cff388
listedmile <listedmile.com>
2026-02-11 11:56:05 +0530

    cache resume error

--- portfolio/globals/links.jsx
+++ portfolio/globals/links.jsx
@@ -6,4 +6,4 @@ export const portfolio = "https://portfoliokunalsharma.vercel.app/";
 export const linkedIn =
   "https://www.linkedin.com/in/kunal-sharma-5a3a27295?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app";
 export const gitHub = "https://github.com/LISTEDMILE";
-export const resume = "/resume.pdf";
+export const resume = "/reesume.pdf";
