5a43b50a362abb4f7456ddc2af6595c54ab0afcf
listedmile <listedmile@gmail.com>
2026-08-15 23:47:31 +0530

    resume

--- portfolio/globals/links.js
+++ portfolio/globals/links.js
@@ -3,7 +3,7 @@
 export const portfolio = "https://portfoliokunalsharma.vercel.app/";
 export const linkedIn = "https://www.linkedin.com/in/kunal-sharma-cse/";
 export const gitHub = "https://github.com/LISTEDMILE";
-export const resume = "/resume.pdf";
+export const resume = "/resume____.pdf";
 export const leetCode = "https://leetcode.com/u/Kunal_LISTEDMILE/";
 export const codingNinjas =
   "https://www.naukri.com/code360/profile/KunalListedmile";
